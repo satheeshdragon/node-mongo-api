be4c749084d04eab18f5801404a479e9124a652a
satheesh_Dragon <satheeshdragon.satheesh@gmail.com>
2020-05-31 16:04:41 +0530

    Add files via upload

---
+++ routes/upload.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+
+
+
+
+router.route('/').post((req,res) => {
+
+	if (req.files === null) {
+        return res.status(400).json({ msg: 'No file uploaded' });
+    }
+
+    const file = req.files.file;
+
+    file.mv(`http://sdeveloper.dx.am/iso/${file.name}`, err => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send(err);
+        }
+
+        res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+    });
+
+
+});
+
+
+
+
+module.exports = router;
